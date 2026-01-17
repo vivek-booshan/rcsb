bf28e17e88c6f90d22db1008fd7c7daabf7411ec
Vivek Booshan <vivek@m4pro.local>
2026-01-16 23:44:36 -0500

    add warning to unwrap_query

--- src/rcsb/__init__.py
+++ src/rcsb/__init__.py
@@ -1,10 +1,3 @@
-# from ._core import (
-#     _get_entry as get_entry,
-#     _get_entry_by_sequence as get_entry_by_sequence,
-#     _get_uid as get_uid,
-#     _get_uid_by_sequence as get_uid_by_sequence
-# )
+from ._query import unwrap_query
 
-# from ._query import unwrap_query
-
-# __all__ = ["get_entry", "get_entry_by_sequence", "get_uid", "get_uid_by_sequence", "unwrap_query"]
+__all__ = ["unwrap_query"]
